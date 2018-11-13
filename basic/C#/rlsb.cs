8f2c7f034e9ae0c2ee3104f8ebbd84cc10f91362
zhengjim <827301757@qq.com>
2018-11-13 09:52:50 +0800

    添加RLSB技术C#实现版

---
+++ basic/C#/rlsb.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("荣林傻逼");
+        }
+    }
+}
